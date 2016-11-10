0e15349914b621505ad8d7a66ac6076c99d06338
kassir <saroukoukassir@yahoo.fr>
2016-11-10 04:11:29 +0100

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,6 +16,7 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            
         ));
     }
 }
