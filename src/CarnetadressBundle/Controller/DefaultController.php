c0d04e395165c011d91e80c0f88f29d5a9bc934b
skassir <saroukoukassir@yahoo.fr>
2016-11-10 03:20:24 +0100

    test

--- src/CarnetadressBundle/Controller/DefaultController.php
+++ src/CarnetadressBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-      $message = 'Bienvenue sur mon carnet d\'adresse';
+      $message = 'Vous êtes sur mon carnet d\'adresse';
 
   		return $this->container->get('templating')->renderResponse('CarnetadressBundle:Default:index.html.twig',
     	array('message' => $message)
